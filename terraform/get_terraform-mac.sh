1490a29498a12b29c498d45fd4d0d913ba4cfcaf
garrettwong <garrettwong@outlook.com>
2020-03-12 01:38:19 -0700

    update gitignore

---
+++ terraform/get_terraform-mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+wget https://releases.hashicorp.com/terraform/0.12.23/terraform_0.12.23_darwin_amd64.zip
+
+unzip terraform_0.12.23_darwin_amd64.zip
+
+chmod +x terraform
+
+rm terraform_0.12.23_darwin_amd64.zip
+
